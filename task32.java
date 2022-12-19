7b9c3b1435fc1e2b54f6e73f8a0615fe9ecc3b03
MargaritaVoroncova <margaritavrncv@gmail.com>
2022-12-19 14:48:24 +0300

    even

---
+++ task32.java
@@ -0,0 +1,17 @@
+//Пусть дан произвольный список целых чисел, удалить из него четные числа
+import java.util.ArrayList;
+import java.util.Random;
+
+public class task32 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> digits = new ArrayList<>();
+        for (int i = 0; i < 15; i++)
+            digits.add(rnd.nextInt(10));
+        System.out.println(digits);
+        for (int i = 0; i < digits.size(); i++)
+            if(digits.get(i)%2!=0)digits.remove(i--);
+        System.out.println(digits);
+    }
+}
+
